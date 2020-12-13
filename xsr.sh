1350a4da15818a666ae527a7b1e7e275e06fe483
Olivier <olivier@lab-concepts.com>
2020-12-13 16:18:49 +0100

    Improve bash file : export with pandoc and zip work

--- xsr.sh
+++ xsr.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
+# ==================================================================
+#                            INIT
+# ==================================================================
+
 # define constants
 declare -r TRUE=0
 declare -r FALSE=1
 
 # Script directory
 thedir=`dirname $0`
-cd $thedir
-thedir=`pwd`
+cd "$thedir"
+#thedir=`pwd`
 
 # default file names
 filename="save.html"
@@ -15,12 +19,16 @@ imageditor="gwenview"
 htmleditor="/home/olivier/Logiciels/bluegriffon/bluegriffon"
 fileexplorer="dolphin"
 cssarticle="css/article.css"
-watermark="/home/olivier/Logiciels/xsr/logo.png"
+watermark="icons/logo.png"
 
 # color definition
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+
+# ==================================================================
+#                         FUNCTIONS
+# ==================================================================
 
 # check command
 function isCommandeExist() {
@@ -59,6 +67,57 @@ function displayMessage() {
     echo "--------------------------------------------------------------"
 }
 
+# convert HTML file to MarkDown
+function convertFileToMarkdownThenTo() {
+    filename="$1"
+    conversion="$2"
+
+    thehtmldir=`dirname $filename`
+    name=`basename "$filename" .html`
+
+    cd "$thehtmldir"
+
+    pandoc -s -f html -t markdown "$name.html" > "$name.md"
+    sed -i '/div/d' "$name.md"
+    sed -i "s/file:\/\/.*\/$name\//$name\//" "$name.md"
+
+    case $conversion in
+        "pdf")
+            pandoc -s -o "$name.pdf" "$name.md"
+            rm "$name.md"
+            ;;
+        "odt")
+            pandoc -s -o "$name.odt" "$name.md"
+            rm "$name.md"
+            ;;
+        "docx")
+            pandoc -s -o "$name.docx" "$name.md"
+            rm "$name.md"
+            ;;
+        *)
+            ;;
+    esac
+}
+
+# zip data
+function zipData() {
+    filename="$1"
+
+    thehtmldir=`dirname $filename`
+    name=`basename "$filename" .html`
+
+    cd $thehtmldir
+
+    hasZip=$(isCommandeExist "zip");
+
+    [ $hasZip -eq $TRUE ] && zip -r "$name.zip" "$name/" "$name.html" || displayMessage "[WARN] install zip to compress data" 3
+}
+
+
+# ==================================================================
+#                         MAIN PROCESS
+# ==================================================================
+
 hasZenity=$(isCommandeExist "zenity");
 hasPandoc=$(isCommandeExist "pandoc");
 
@@ -83,12 +142,12 @@ if [ "$filename" != "" ]; then
         # choose mode
         mode=$(zenity --list --title="2 - Mode" --text "Which mode ?" --radiolist --height=270 --width=850\
                     --column "Choice" --column "Mode" --column "Description" \
-                    FALSE "Tutorial full" "on edite tout : image + html + fichier"\
-                    TRUE "Tutorial mail" "chemin absolu des images pour copier coller dans un mail (-p) + on edite : image + html"\
-                    FALSE "Tutorial watermark" "chemin absolu des images pour copier coller dans un mail (-p) + on edite : image + html"\
-                    FALSE "Tutorial zip" "chemin relatif des images pour pouvoir déplacer le HTML (pas de -p) + on edite : image + html"\
-                    FALSE "Manual" "chemin relatif des images + selection + on edite : image + html"\
-                    FALSE "Article" "chemin relatif des images (pas de -p) et mode article (css article) avec legende (-g) + on edite : image + html"\
+                    FALSE "Tutorial full" "Full edition : image + html + files"\
+                    TRUE "Tutorial mail" "Absolute path of the images to copy paste in an email (-p) + edit: image + html"\
+                    FALSE "Tutorial watermark" "Absolute path of the images to copy paste in an email (-p) + watermark + edit: image + html"\
+                    FALSE "Tutorial zip" "Relative path of images to be able to move the HTML (no -p) + edit: image + html + zip compression"\
+                    FALSE "Manual" "Relative path of images + selection of screenshots + edit: image + html"\
+                    FALSE "Article" "Relative path of images (no -p) and article mode (css article) with legend (-g) + edit: image + html"\
                     2> /dev/null);
     fi
 
@@ -97,33 +156,31 @@ if [ "$filename" != "" ]; then
 
         case $mode in
             "Tutorial full")
-                #- 1 -# on edite tout : image + html + fichier
-                #echo ">>> Mode tuto avec chemin absolu image et screenshot focus seulement puis edition image, HTML et fichier final"
+                echo ">>> Full edition : image + html + files"
                 ./bin/xsr.pl "$filename" --lang=fr -p -e --images-editor="$imageditor" -s --html-editor="$htmleditor" -r --file-explorer="$fileexplorer"
                 ;;
             "Tutorial mail")
-                #- 2 -# chemin absolu des images pour copier coller dans un mail (-p) + on edite : image + html
-                echo ">>> Mode tuto avec chemin absolu image et screenshot focus seulement puis edition image et HTML"
+                echo ">>> Absolute path of the images to copy paste in an email (-p) + edit: image + html"
                 ./bin/xsr.pl "$filename" --lang=fr -p -e --images-editor="$imageditor" -s --html-editor="$htmleditor"
                 ;;
             "Tutorial watermark")
                 #- 2 -# chemin absolu des images pour copier coller dans un mail (-p) + watermark + on edite : image + html
-                echo ">>> Mode tuto avec chemin absolu image, screenshot focus seulement et watermark puis edition image et HTML"
+                echo ">>> Absolute path of the images to copy paste in an email (-p) + watermark + edit: image + html"
                 ./bin/xsr.pl "$filename" --lang=fr -p -e -a --watermark="$watermark" --images-editor="$imageditor" -s --html-editor="$htmleditor"
                 ;;
             "Tutorial zip")
                 #- 3 -# chemin relatif des images pour pouvoir déplacer le HTML (pas de -p) + on edite : image + html
-                #echo ">>> Mode tuto avec chemin relatif image et screenshot focus seulement puis edition image et HTML"
+                echo ">>> Relative path of images to be able to move the HTML (no -p) + edit: image + html + zip compression"
                 ./bin/xsr.pl "$filename" --lang=fr -e --images-editor="$imageditor" -s --html-editor="$htmleditor"
                 ;;
             "Manual")
                 #- 2 -# chemin relatif des images + selection + on edite : image + html
-                echo ">>> Mode tuto avec chemin absolu image et screenshot focus seulement puis edition image et HTML"
+                echo ">>> Relative path of images + selection of screenshots + edit: image + html"
                 ./bin/xsr.pl "$filename" --lang=fr -e --screenshot-mode="select" --images-editor="$imageditor" -s --html-editor="$htmleditor"
                 ;;
             "Article")
                 #- 4 -# chemin relatif des images (pas de -p) et mode article (css article) avec legende (-g) + on edite : image + html
-                #echo ">>> Mode article avec chemin absolu image, screenshot focus seulement et legende image puis edition image et HTML"
+                echo ">>> Relative path of images (no -p) and article mode (css article) with legend (-g) + edited: image + html"
                 ./bin/xsr.pl "$filename" --lang=fr -p -g --css="$cssarticle" -e --images-editor="$imageditor" -s --html-editor="$htmleditor"
                 ;;
             *)
@@ -144,6 +201,7 @@ if [ "$filename" != "" ]; then
                             FALSE "md" "Markdown file"\
                             FALSE "zip" "Create zip archive"\
                             FALSE "odt" "Open Office Writer file"\
+                            FALSE "docx" 'Micro$oft Word file'\
                             2> /dev/null);
         fi
 
@@ -152,17 +210,20 @@ if [ "$filename" != "" ]; then
                 displayMessage "[INFO] No conversion for file $filename" 2
                 ;;
             "pdf")
-                pandoc -s -f html -t pdf $filename > $filename.md
+                convertFileToMarkdownThenTo "$filename" 'pdf'
                 ;;
             "md")
-                pandoc -s -f html -t markdown $filename > $filename.md
-                sed -i '/div/d' $filename.md
+                convertFileToMarkdownThenTo "$filename"
                 ;;
             "zip")
                 # todo : compress html and directory
+                zipData "$filename"
                 ;;
             "odt")
-                pandoc -s -f html -t odt $filename > $filename.md
+                convertFileToMarkdownThenTo "$filename" 'odt'
+                ;;
+            "docx")
+                convertFileToMarkdownThenTo "$filename" 'docx'
                 ;;
             *)
                 #echo "Unknown conversion : $conversion"
@@ -180,4 +241,3 @@ if [ "$filename" != "" ]; then
         read
     fi
 fi
-
